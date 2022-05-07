11880ab7feb39f3ffec332271aacbe60fffb6d76
YuraRiabov <uraryabov@gmail.com>
2022-05-07 14:55:03 +0300

    Cell class implemented

---
+++ ChessPieces/Models/Cell.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPieces.Models
+{
+    internal class Cell
+    {
+        private int _rowIndex;
+        private int _columnIndex;
+        public int RowIndex
+        { 
+            get => _rowIndex;
+            set
+            {
+                if (value >= 0 && value < 8)
+                {
+                    _rowIndex = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+            }
+        }
+        public int ColumnIndex
+        {
+            get => _columnIndex;
+            set
+            {
+                if (value >= 0 && value < 8)
+                {
+                    _columnIndex = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+            }
+        }
+        public Cell(int row, int column)
+        {
+            RowIndex = row;
+            ColumnIndex = column;
+        }
+        public string RowToString()
+        {
+            return RowIndex switch
+            {
+                0 => "a",
+                1 => "b",
+                2 => "c",
+                3 => "d",
+                4 => "e",
+                5 => "f",
+                6 => "g",
+                7 => "h",
+                _ => ""
+            };
+        }
+        public string ColumnToString()
+        {
+            return (ColumnIndex + 1).ToString();
+        }
+        public override string ToString()
+        {
+            return RowToString() + ColumnToString();
+        }
+    }
+}
