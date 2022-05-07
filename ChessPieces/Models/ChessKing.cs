6ddbc3506d3210f1143a8fa58789c286afb2b062
YuraRiabov <uraryabov@gmail.com>
2022-05-07 15:42:34 +0300

    ChessKing class created, tamplate method for checking cell reachability implemented

---
+++ ChessPieces/Models/ChessKing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPieces.Models
+{
+    internal class ChessKing : ChessPiece
+    {
+        protected override bool IsReachable(Cell cell)
+        {
+            return Location.IsNeighbour(cell);
+        }
+    }
+}
