13697503beae8b45091a44c5c68f9821598d8f3c
YuraRiabov <uraryabov@gmail.com>
2022-05-07 16:09:26 +0300

    Bishop class created and template method implemented

---
+++ ChessPieces/Models/ChessBishop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPieces.Models
+{
+    internal class ChessBishop : ChessPiece
+    {
+        protected override bool IsReachable(Cell cell)
+        {
+            return Location.OnSameDiagonal(cell);
+        }
+    }
+}
