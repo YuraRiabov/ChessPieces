f7b8650b2baca1ff606c1e9f14aeaa22a8d0758b
YuraRiabov <uraryabov@gmail.com>
2022-05-07 16:11:35 +0300

    Knight class created and template method implemented

---
+++ ChessPieces/Models/ChessKnight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPieces.Models
+{
+    internal class ChessKnight : ChessPiece
+    {
+        protected override bool IsReachable(Cell cell)
+        {
+            return Location.WithinKnightReach(cell);
+        }
+    }
+}
