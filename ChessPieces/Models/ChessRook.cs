72a2b5d5e62f9c16cfe8e8bffd7e0147bbd7d913
YuraRiabov <uraryabov@gmail.com>
2022-05-07 16:06:53 +0300

    ChessRook class implemented and template method implemented

---
+++ ChessPieces/Models/ChessRook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPieces.Models
+{
+    internal class ChessRook : ChessPiece
+    {
+        protected override bool IsReachable(Cell cell)
+        {
+            return Location.InSameRow(cell) || Location.InSameColumn(cell);
+        }
+    }
+}
