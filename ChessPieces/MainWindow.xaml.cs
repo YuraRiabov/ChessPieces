b589076a15549df7e54da86b6125e11ac9bdebeb
YuraRiabov <uraryabov@gmail.com>
2022-05-08 12:26:21 +0300

    View model for main window added with some mock data and binded to the view

--- ChessPieces/MainWindow.xaml.cs
+++ ChessPieces/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessPieces.ViewModels;
 
 namespace ChessPieces
 {
@@ -20,6 +21,7 @@ namespace ChessPieces
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindowViewModel ViewModel { get; set; } = new MainWindowViewModel();
         public MainWindow()
         {
             InitializeComponent();
---
+++ ChessPieces/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessPieces.Enums;
+using ChessPieces.Models;
+
+namespace ChessPieces.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        List<(ChessPieceTypeEnum, int, int)> mockPieces = new List<(ChessPieceTypeEnum, int, int)>
+        {
+            (ChessPieceTypeEnum.Queen, 1, 1),
+            (ChessPieceTypeEnum.Rook, 2, 2),
+            (ChessPieceTypeEnum.King, 3, 3),
+            (ChessPieceTypeEnum.Bishop, 2, 4),
+            (ChessPieceTypeEnum.Knight, 1, 4)
+        };
+        public string Captures { get; set; }
+        private ChessBoard _chessBoard;
+        public MainWindowViewModel()
+        {
+            _chessBoard = new ChessBoard(mockPieces);
+            Captures = "Possible captures:\n" + DataConverter.CapturesToString(_chessBoard.Captures);
+        }
+
+    }
+}
