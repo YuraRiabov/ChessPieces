ccacc2835ef9daea3a2e9c1644617b6ac18c3a79
YuraRiabov <uraryabov@gmail.com>
2022-05-08 15:03:10 +0300

    Command classes added

---
+++ ChessPieces/Commands/RelayCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPieces.Commands
+{
+    public class RelayCommand<T> : CommandBase
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+        public override bool CanExecute(object? parameter) => _canExecute((T)parameter);
+        public override void Execute(object? parameter) => _execute((T)parameter);
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+    }
+}
